39e171c7bc947782915819e8928d30751e1309ca
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2023-01-30 21:07:42 +0700

    gf

---
+++ resources/views/Home/index.blade.php
@@ -0,0 +1,5 @@
+@extends('Sidebar.sidebar')
+@section('title','Home')
+@section('content')
+    <h1>Content home</h1>
+@endsection
---
+++ resources/views/Sidebar/sidebar.blade.php
@@ -0,0 +1,5 @@
+@extends('Layout.master')
+{{-- @section('title','Home') --}}
+@section('sidebar')
+    <h1>Side bar</h1>
+@endsection
