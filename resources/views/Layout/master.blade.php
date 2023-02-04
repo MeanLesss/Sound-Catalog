6ae2322922dbb3f3f68972c8d1de926184a1be3c
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2023-02-04 13:16:09 +0700

    align sound content in place

--- resources/views/Layout/master.blade.php
+++ resources/views/Layout/master.blade.php
@@ -17,6 +17,7 @@
         }
 
         body {
+            box-sizing: unset;
             font-family: 'Kodchasan';
             margin: 0;
             padding: 0;
@@ -24,10 +25,10 @@
                     rgba(181, 55, 242, 1) 61%, rgba(138, 43, 226, 1) 100%);
         }
 
-        .content {
+        /* .content {
             width: 100%;
             height: 100%;
-        }
+        } */
 
         .sidebar {
             width: 20%;
@@ -111,19 +112,20 @@
 
 <body>
     {{-- <h1>MeanLess Sound</h1> --}}
-    <div style="height: 100vh;width:100vw;">
-        <div class="d-flex m-0 p-0" style="width:100%;height:100%">
-            {{-- sidebar block --}}
-            <div class="d-flex flex-column justify-content-center sidebar">
-                @yield('sidebar')
-            </div>
+    {{-- <div style="height: 100vh;width:100vw;"> --}}
+    <div class="d-flex" style="width:100vw;height:100vh">
+        {{-- sidebar block --}}
+        <div class="d-flex flex-column justify-content-center sidebar">
+            @yield('sidebar')
+        </div>
 
-            {{-- content block --}}
-            <div class="d-flex flex-column justify-content-start align-items-center p-3 m-0 content gap-3">
-                @yield('content')
-            </div>
+        {{-- content block style="width: 100%;height: 100%;"--}}
+        <div class="d-flex flex-column justify-content-start px-5 m-0" style="width: 100%;">
+            {{-- <div class="d-flex flex-column justify-content-start align-items-center p-3 m-0 container"> --}}
+            @yield('content')
         </div>
     </div>
+    {{-- </div> --}}
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
     </script>
--- resources/views/Sound/index.blade.php
+++ resources/views/Sound/index.blade.php
@@ -1,13 +1,13 @@
 @extends('Sidebar.sidebar')
 @section('title', 'sound')
 @section('content')
-    <div class="" style="overflow: auto">
-        <h1>Find the sound suit your project.</h1>
-        <div class="d-flex justify-content-center align-items-start">
-            {{-- input type="text" class="form-control" placeholder="Search" aria-label="Large" aria-describedby="inputGroup-sizing-sm"> --}}
-            <div class="input-group mb-3">
-                <div class="input-group-prepend dropdown">
-                    {{-- <button class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown"
+
+    <h1>Find the sound suit your project.</h1>
+    <div class="d-flex justify-content-center align-items-start">
+        {{-- input type="text" class="form-control" placeholder="Search" aria-label="Large" aria-describedby="inputGroup-sizing-sm"> --}}
+        <div class="input-group mb-3">
+            <div class="input-group-prepend dropdown">
+                {{-- <button class="btn btn-outline-secondary dropdown-toggle" data-toggle="dropdown"
                     aria-haspopup="true" aria-expanded="true">Dropdown</button>
                     <div class="dropdown-menu">
                         <a class="dropdown-item" href="#">Action</a>
@@ -16,14 +16,17 @@
                         <div role="separator" class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#">Separated link</a>
                     </div> --}}
-                </div>
-                <input type="text" class="rounded form-control" placeholder="Search your sound"
-                    aria-label="Text input with dropdown button">
-                <a class="btn btn-outline-secondary" href="#">Search</a>
             </div>
+            <input type="text" class="rounded form-control" placeholder="Search your sound"
+                aria-label="Text input with dropdown button">
+            <a class="btn btn-outline-secondary" href="#">Search</a>
         </div>
+    </div>
+    {{-- Cards container  --}}
+    <div class="d-flex flex-row flex-wrap gap-3" style="overflow: auto">
+
         <!-- Card -->
-        <div class="card" style="width: 300px;height: 500px;">
+        <div class="card" style="width: 350px;height: 500px;">
             <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
                 <img class="card-img-top" src="https://mdbootstrap.com/wp-content/uploads/2019/02/flam.jpg"
                     alt="Card image cap">
@@ -36,19 +39,155 @@
                 <h5 class="h5 font-weight-bold"><a href="#" target="_blank">Dj Flam</a></h5>
                 <p class="mb-0">Urban Bachata remix</p>
 
-                <audio id="music" preload="false" controls>
+                <audio id="music" preload="true" controls>
                     <source src="{{ asset('sounds\\Winning Sound.mp3') }}" type="audio/mpeg">
                 </audio>
-                <div id="audioplayer">
+                {{-- <div id="audioplayer">
                     <i id="pButton" class="fas fa-play"></i>
                     <div id="timeline">
                         <div id="playhead"></div>
                     </div>
-                </div>
+                </div> --}}
 
             </div>
         </div>
         <!-- Card -->
-        {{-- <audio id="indie" src="{{ asset('sounds\\Winning Sound.mp3') }}" preload="auto" controls></audio> --}}
+        <!-- Card -->
+        <div class="card" style="width: 350px;height: 500px;">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img class="card-img-top" src="https://mdbootstrap.com/wp-content/uploads/2019/02/flam.jpg"
+                    alt="Card image cap">
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+            <div class="card-body text-center">
+
+                <h5 class="h5 font-weight-bold"><a href="#" target="_blank">Dj Flam</a></h5>
+                <p class="mb-0">Urban Bachata remix</p>
+
+                <audio id="music" preload="true" controls>
+                    <source src="{{ asset('sounds\\Winning Sound.mp3') }}" type="audio/mpeg">
+                </audio>
+                {{-- <div id="audioplayer">
+                    <i id="pButton" class="fas fa-play"></i>
+                    <div id="timeline">
+                        <div id="playhead"></div>
+                    </div>
+                </div> --}}
+
+            </div>
+        </div>
+        <!-- Card -->
+        <!-- Card -->
+        <div class="card" style="width: 350px;height: 500px;">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img class="card-img-top" src="https://mdbootstrap.com/wp-content/uploads/2019/02/flam.jpg"
+                    alt="Card image cap">
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+            <div class="card-body text-center">
+
+                <h5 class="h5 font-weight-bold"><a href="#" target="_blank">Dj Flam</a></h5>
+                <p class="mb-0">Urban Bachata remix</p>
+
+                <audio id="music" preload="true" controls>
+                    <source src="{{ asset('sounds\\Winning Sound.mp3') }}" type="audio/mpeg">
+                </audio>
+                {{-- <div id="audioplayer">
+                    <i id="pButton" class="fas fa-play"></i>
+                    <div id="timeline">
+                        <div id="playhead"></div>
+                    </div>
+                </div> --}}
+
+            </div>
+        </div>
+        <!-- Card -->
+        <!-- Card -->
+        <div class="card" style="width: 350px;height: 500px;">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img class="card-img-top" src="https://mdbootstrap.com/wp-content/uploads/2019/02/flam.jpg"
+                    alt="Card image cap">
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+            <div class="card-body text-center">
+
+                <h5 class="h5 font-weight-bold"><a href="#" target="_blank">Dj Flam</a></h5>
+                <p class="mb-0">Urban Bachata remix</p>
+
+                <audio id="music" preload="true" controls>
+                    <source src="{{ asset('sounds\\Winning Sound.mp3') }}" type="audio/mpeg">
+                </audio>
+                {{-- <div id="audioplayer">
+                    <i id="pButton" class="fas fa-play"></i>
+                    <div id="timeline">
+                        <div id="playhead"></div>
+                    </div>
+                </div> --}}
+
+            </div>
+        </div>
+        <!-- Card -->
+        <!-- Card -->
+        <div class="card" style="width: 350px;height: 500px;">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img class="card-img-top" src="https://mdbootstrap.com/wp-content/uploads/2019/02/flam.jpg"
+                    alt="Card image cap">
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+            <div class="card-body text-center">
+
+                <h5 class="h5 font-weight-bold"><a href="#" target="_blank">Dj Flam</a></h5>
+                <p class="mb-0">Urban Bachata remix</p>
+
+                <audio id="music" preload="true" controls>
+                    <source src="{{ asset('sounds\\Winning Sound.mp3') }}" type="audio/mpeg">
+                </audio>
+                {{-- <div id="audioplayer">
+                    <i id="pButton" class="fas fa-play"></i>
+                    <div id="timeline">
+                        <div id="playhead"></div>
+                    </div>
+                </div> --}}
+
+            </div>
+        </div>
+        <!-- Card -->
+        <!-- Card -->
+        <div class="card" style="width: 350px;height: 500px;">
+            <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img class="card-img-top" src="https://mdbootstrap.com/wp-content/uploads/2019/02/flam.jpg"
+                    alt="Card image cap">
+                <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+            </div>
+            <div class="card-body text-center">
+
+                <h5 class="h5 font-weight-bold"><a href="#" target="_blank">Dj Flam</a></h5>
+                <p class="mb-0">Urban Bachata remix</p>
+
+                <audio id="music" preload="true" controls>
+                    <source src="{{ asset('sounds\\Winning Sound.mp3') }}" type="audio/mpeg">
+                </audio>
+                {{-- <div id="audioplayer">
+                    <i id="pButton" class="fas fa-play"></i>
+                    <div id="timeline">
+                        <div id="playhead"></div>
+                    </div>
+                </div> --}}
+
+            </div>
+        </div>
+        <!-- Card -->
     </div>
+    {{-- <audio id="indie" src="{{ asset('sounds\\Winning Sound.mp3') }}" preload="auto" controls></audio> --}}
+
 @endsection
