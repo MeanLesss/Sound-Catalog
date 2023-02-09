59c9b52688796968dd039324a32249e25ba12381
MeanLesss <68323176+MeanLesss@users.noreply.github.com>
2023-02-09 22:24:54 +0700

    wer

--- resources/views/Sound/addSound.blade.php
+++ resources/views/Sound/addSound.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Add sound')
 @section('content')
     @if (auth()->check())
-        {{auth()->user()->id}}
+        {{-- {{auth()->user()->id}} --}}
 
         <div class="d-flex justify-content-center align-items-center " style="height: 100%;">
             {!! Form::open(['route' => 'sound.add', 'method' => 'POST', 'style' => 'max-width:50%;width:30%;']) !!}
@@ -26,11 +26,21 @@
                 <input type="textarea" class="form-control" name="description" id="floatingInput" placeholder="Description">
                 <label for="floatingInput">Description</label>
             </div>
+            <div class="form-floating mb-3">
+                <select class="form-select" multiple data-mdb-placeholder="Example placeholder" multiple>
+                    <option selected value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                    <option value="4">Four</option>
+                    <option value="5">Five</option>
+                  </select>
+            </div>
             <button class="w-100 btn btn-lg btn-danger" type="submit">Upload</button>
             {!! Form::close() !!}
         </div>
     @else
-    <div class="d-flex justify-content-center align-items-center content" >
+    <div class="d-flex flex-column justify-content-center align-items-center content" >
+        <h1>Plase log in before upload your sound!</h1>
         <a href="/login" class="btn btn-danger">Go to login</a>
     </div>
     @endif
